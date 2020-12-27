a0d771f1d876ec99e63a4cad758d3f7f1c37e538
chris2in <lyh2in@gmail.com>
2020-12-26 20:49:24 -0600

    update

--- easy.py
+++ easy.py
@@ -4,8 +4,12 @@ while True:
     item = input("Enter item name:")
     if item =="0" or item == "":
         break
+
     price = input("enter price:")
-    tag =("<button type = \"button\" class =\"collapsible\">{}  ${}</button> \n<div class=\"content\">\n<img >\n  </div>").format(item,price)
+    if(input()=="1"):
+        tag =("<button type = \"button\" class =\"collapsibleSPICY\">{}  ${}</button> \n<div class=\"content\">\n<img >\n  </div>").format(item,price)
+    else:
+        tag =("<button type = \"button\" class =\"collapsible\">{}  ${}</button> \n<div class=\"content\">\n<img >\n  </div>").format(item,price)
     print(tag)
     current = current +"\n"+tag
     pyperclip.copy(current)
